4b468e8257ac71031f739e0e00e9936abed7bc6f
Aria Gomes <aria.gomes00@gmail.com>
2023-11-08 20:07:23 -0500

    updated config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'raw.githubusercontent.com',
-            'steamcdn-a.akamaihd.net',
-            'community.cloudflare.steamstatic.com'
-        ],
+      domains: [
+        'raw.githubusercontent.com',
+        'steamcdn-a.akamaihd.net',
+        'community.cloudflare.steamstatic.com',
+      ],
     },
-}
-
-module.exports = nextConfig
+    webpack: (config, { isServer }) => {
+      // This will ensure that the json file is included in the server-side build
+      if (isServer) {
+        config.node = {
+          __dirname: true,
+        };
+      }
+  
+      return config;
+    },
+    staticPageGenerationTimeout: 120000,
+  };
+  
+  module.exports = nextConfig;
